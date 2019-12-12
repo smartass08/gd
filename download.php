f6b767ed0a10cd5283a3e46532fc7e75592da091
idwifi <56871212+idwifi@users.noreply.github.com>
2019-12-12 13:18:05 +0700

    Update download.php

--- download.php
+++ download.php
@@ -1,3 +1,4 @@
+*/ Google Drive Direct Component by st4zz */
 <?php
 $clean = fopen('error_log', 'w');
 fwrite($clean,'');
@@ -38,39 +39,5 @@ $id = $_GET['gd'];
 		}
 }
 $docsurl = udud($id);
-
-if(empty($_GET['gd']){
-echo '<!DOCTYPE html>
-<html style="height:100%">
-<head>
-    <title>404 Not Found :( &#8211; SaRDrive</title>
-    <meta name="description" content="Error Content Not Found"/>
-    <link rel="canonical" href="https://sardrive.com/" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="404 Not Found :( &#8211; SaRDrive" />
-    <meta property="og:description" content="Error Content Not Found" />
-    <meta property="og:url" content="https://sardrive.com/" />
-    <meta property="og:site_name" content="404 Not Found :( &#8211; SaRDrive" />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:description" content="Error Content Not Found" />
-    <meta name="twitter:title" content="SaRDrive - Safely store and share your photos, videos, files and more in the cloud." />
-	<meta name="theme-color" content="#E52775" />
-	<meta name="msapplication-navbutton-color" content="#E52775" />
-	<meta name="msapplication-TileColor" content="#E52775"/>
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="#E52775" />
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <link href=\'https://fonts.googleapis.com/css?family=Ubuntu\' rel=\'stylesheet\' />
-</head>
-<body style="color: #444; margin-top:15px;font: normal 14px/20px Arial, Helvetica, sans-serif; height:50%; background-color: #fff;">
-<h1 style="font-family:Ubuntu;margin:6px;padding:2px;">404 File Not Found...!!!</h1><br/>
-<p style="font-family:Ubuntu;margin:6px;padding:2px;font-weight:bold;">Reason:<br/>- File deleted by Administrator<br/>- Wrong URL Generator<br/>- Account with all files suspended/deleted by host provider<br/>- Deleted because complaint DMCA</p>
-<p style="font-family:Ubuntu;margin:6px;padding:2px;font-weight:bold;"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfzTYKY-UAOUN1xIhoYXtZ52c-bmeBNvleBURs7z6eCJJwLzg/viewform" title="Request" target="_blank"><strong>&#x27A4; Request Reupload Here..!!</strong></a></p>
-</body>
-</html>';
-}else{
 header("location: $docsurl");
-}
 ?>
